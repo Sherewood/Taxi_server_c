84b32924fd7027aedee48a5b8c81f7d4c540e1c5
Sherewood <52391353+Sherewood@users.noreply.github.com>
2020-03-30 22:02:52 -0400

    Add files via upload

--- a5/customer.c
+++ a5/customer.c
@@ -13,9 +13,44 @@
 // This represents a customer request.  There should be two command line arguments representing the
 // pickup location and dropoff location.  Each should be a number from 0 to the NUM_CITY_AREAS.
 void main(int argc, char *argv[]) {
-  int                 clientSocket;  // client socket id
-  struct sockaddr_in  clientAddress; // client address
 
+  int PI=atoi(argv[1]);
+  int DO=atoi(argv[2]);
+  if (PI!=NULL || DO!=NULL){
+    int                 clientS;
+    struct sockaddr_in    address;
+    int                 status, bytesRcv;
 
+    char                buffer[80];
+    clientS = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (clientS< 0) {
+      printf("*** CLIENT ERROR: Could open socket.\n");
+      exit(-1);
+    }
+    printf("***CONNECTED To Customer\n");
+    memset(&address, 0, sizeof(address));
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = inet_addr(SERVER_IP);
+    address.sin_port = htons((unsigned short) SERVER_PORT);
+    // Connect to server
+    status = connect(clientS, (struct sockaddr *) &address, sizeof(address));
+    if (status < 0) {
+      printf("*** CLIENT ERROR: hgj Could not connect.\n");
+      exit(-1);
+    }
+    if (PI<=NUM_CITY_AREAS || PI>=0 && DO<=NUM_CITY_AREAS ||DO>=0){
+      printf("You entered good coordinates %d and %d\n", PI, DO);
+    buffer[0]=REQUEST_TAXI;
+    buffer[1]=AREA_NAMES[PI];
+    buffer[2]=AREA_NAMES[DO];
+    send(clientS, buffer, strlen(buffer), 0);
+    }
+    else{
+      printf("Your request was invalid or the server is full\n" );
+    }
+  }
+  else{
+    printf("you have not entered enough details");
+
+  }
 }
-
--- a5/generator.c
+++ a5/generator.c
@@ -9,10 +9,16 @@ void main() {
   srand(time(NULL));
 
   while(1) {
-    // ...
+    int PI= rand()%6;
+    int DO= rand()%6;
+    char customer[100];
+    sprintf(customer, "./customer %d %d", PI, DO);
+    system(customer);
     // add code here
     // ...
-    
+  //  system(./customer);
+
     usleep(250000);   /// do not remove this
   }
 }
+//system("./customer 0 5");
